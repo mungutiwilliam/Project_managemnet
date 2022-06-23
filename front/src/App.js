fb5ec0c226cd29d5a8a7148b8fdcef470c58db49
Alexander-clemence <51910485+Alexander-clemence@users.noreply.github.com>
2022-06-23 12:39:58 +0300

    navbar

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Navbar from './Pages/Navbar/Navbar';
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
-  );
+    <BrowserRouter>
+      <Navbar/>
+  
+      <Routes>
+
+      </Routes>
+    </BrowserRouter>
+  
+    );
 }
 
 export default App;
