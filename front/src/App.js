6b6e354ab1ff261af80a22d8ef48a0b27c0a7cb1
Alexander-clemence <51910485+Alexander-clemence@users.noreply.github.com>
2022-06-30 09:08:05 +0300

    Owner Side

--- front/src/App.js
+++ front/src/App.js
@@ -7,6 +7,7 @@ import {
 import Navmenu from './Pages/Navbar/Navmenu';
 import Home from './Pages/Home/Home';
 import Mgent from './Pages/Users/Agent/Mgent';
+import Owners from './Pages/Users/Owners/Owners';
 
 
 
@@ -18,6 +19,7 @@ function App() {
       <Routes>
       <Route exact path="/" element={ <Home/> } />
       <Route exact path="/agent" element={ <Mgent/> } />
+      <Route exact path="/owner" element={ <Owners/> } />
       </Routes>
     </BrowserRouter>
   
---
+++ front/src/Pages/Users/Owners/Owners.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Owners() {
+  return (
+    <div>Owners</div>
+  )
+}
+
+export default Owners
